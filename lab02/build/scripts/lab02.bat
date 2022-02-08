325c3357a2cc9fccbdca278ea8e594e2ec19942e
root <root@LAPTOP-ATD3HHUP.localdomain>
2022-02-08 11:11:10 -0500

    updated code

--- lab02/build/scripts/lab02.bat
+++ lab02/build/scripts/lab02.bat
@@ -71,7 +71,7 @@ set CLASSPATH=%APP_HOME%\lib\lab02.jar;%APP_HOME%\lib\guava-30.1.1-jre.jar;%APP_
 
 
 @rem Execute lab02
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %LAB02_OPTS%  -classpath "%CLASSPATH%" Lab02.SimpleTest %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %LAB02_OPTS%  -classpath "%CLASSPATH%" csci2020u.lab02.SimpleTest %*
 
 :end
 @rem End local scope for the variables with windows NT shell
